56909e8d77119100acbf143a6585e2302f027dde
Andre Brito <felipe.brito016@gmail.com>
2023-10-11 21:39:35 -0300

    refactor: added health route

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,10 @@ const app = new Hono()
 
 app.get('/', (c) => c.text('Hello Hono!'))
 
+app.get('health', (c) => c.json({
+  status: 'ok',
+  message: 'Hono is healty',
+  environment: process.env.NODE_ENV,
+}))
+
 export default app
